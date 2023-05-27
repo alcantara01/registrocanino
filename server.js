8de304c98aa09a8a487acac622602693ab2148e9
CristnaAT <alumno.668286@ies-azarquiel.es>
2023-05-27 18:50:07 +0200

    Componentes creados

--- server.js
+++ server.js
@@ -1,30 +1,43 @@
-const express = require('express');
-const mysql = require('mysql');
-
+/* eslint-disable prettier/prettier */
+const express = require("express");
+const mysql = require("mysql");
 const app = express();
 const PORT = 5000; // Puedes cambiar el número de puerto si es necesario
 
 // Configuración de la conexión a la base de datos MySQL
 const connection = mysql.createConnection({
-
-  host: 'localhost',
-  user: 'cris',
-  password: 'Azarquiel2022',
-  database: 'registrocanino',
-  port: 3306 // Puerto por defecto de MySQL en XAMPP
+  host: "localhost",
+  user: "cris",
+  password: "Azarquiel2022",
+  database: "registrocanino",
+  port: 3306, // Puerto por defecto de MySQL en XAMPP
 });
 
 connection.connect((err) => {
   if (err) {
-    console.error('Error al conectar a la base de datos: ', err);
+    console.error("Error al conectar a la base de datos: ", err);
   } else {
-    console.log('Conexión exitosa a la base de datos MySQL');
+    console.log("Conexión exitosa a la base de datos MySQL");
   }
 });
 
 // Ruta de prueba
-app.get('/', (req, res) => {
-  res.send('¡Servidor Express.js en funcionamiento!');
+app.get("/", (req, res) => {
+  res.send("¡Servidor Express.js en funcionamiento!");
+});
+app.get("/perros", (req, res) => {
+  const query =
+    "SELECT id, nombre, fechaNac, pelaje, sexo, nombreMadre, nombrePadre FROM Perro";
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error("Error al obtener los datos de los perros:", err);
+      res
+        .status(500)
+        .json({ error: "Error al obtener los datos de los perros" });
+    } else {
+      res.json(results);
+    }
+  });
 });
 
 // Iniciar el servidor
--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Componentes/Home";
+import Origen from "./Componentes/Origen";
+import Pedigree from "./Componentes/Pedigree";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" component={Home} />
+        <Route path="/origen" component={Origen} />
+        <Route path="/pedigree" component={Pedigree} />
+      </Routes>
+    </Router>
   );
 }
 
